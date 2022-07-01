dc7fc082cabc6f0b9395530055ee8ccaf107f936
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-01 16:25:47 +0700

    refresh redux hands on

---
+++ index.js
@@ -0,0 +1,17 @@
+const redux = require('redux');
+// Source of truth, repository global, database
+const createStore = redux.createStore;
+
+const initialState = {
+    name: 'Tika',
+    age: 23
+}
+
+// Transaksi store
+const rootReducer = (state = initialState, action) => {
+    return state;
+}
+
+// Inisialisasi store
+const store = createStore(rootReducer);
+console.log(store.getState());
