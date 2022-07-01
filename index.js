20dde031f4115fe34ac87fa838a063342247e931
Edward Suwirya <edward_suwirya@yahoo.com>
2022-07-01 16:48:07 +0700

    Redux Action

--- index.js
+++ index.js
@@ -9,9 +9,48 @@ const initialState = {
 
 // Transaksi store
 const rootReducer = (state = initialState, action) => {
+    if (action.type === "ADD_AGE") {
+        // Jangan mutate object, karena redux reducer melakukan perubahan dengan
+        // compare previous state object dengan next state object
+        // https://github.com/reduxjs/redux/blob/master/src/combineReducers.ts
+        // Compare 2 object
+        // let obj1 = {nama: 'Enigma'};
+        // let obj2 = obj1;
+        // obj2.nama = 'Enigmacamp';
+        // console.log(obj1 === obj2); // true
+        // Cara yang salah untuk melakukan update store
+        // state.age = state.age + 1;
+        return {
+            ...state,
+            age: state.age + 1
+        }
+    }
+    if (action.type === "CHANGE_NAME") {
+        return {
+            ...state,
+            name: action.name
+        }
+    }
+
     return state;
 }
 
 // Inisialisasi store
 const store = createStore(rootReducer);
+console.log('State Awal');
+console.log(store.getState());
+store.dispatch(
+    {
+        type: 'ADD_AGE',
+    }
+)
+console.log('State Setelah Tambah Umur');
+console.log(store.getState());
+store.dispatch(
+    {
+        type: 'CHANGE_NAME',
+        name: 'EnigmaCamp'
+    }
+)
+console.log('State Setelah Ganti Nama');
 console.log(store.getState());
